adf5c7d36d2f05ff796f4ee50f8dd04dd813d116
Mitchel-jf <mitchellcorporations@gmail.com>
2020-03-26 11:41:50 +0100

    ADDED GET /todos route

--- server/server.js
+++ server/server.js
@@ -28,8 +28,19 @@ app.post('/todos', (req, res) => {
     });
 });
 
-// app.get('/read');
+//GET/ReadAll endpoint
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (err) => {
+        res.status(400).send({ err });
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+
+module.exports = { app };
